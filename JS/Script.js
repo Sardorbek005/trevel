1dbe115dbb0c8c236604b09f1079897d628b661f
Sardorbek005 <buyukimron2012@gmail.com>
2022-11-23 17:16:40 +0500

    my commit

---
+++ JS/Script.js
@@ -0,0 +1,115 @@
+let signup = document.querySelector('.signup');
+let modal = document.querySelector('.hellow');
+let x = document.querySelector('.x');
+
+signup.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+x.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
+
+
+function ClickBtn(index) {
+
+    if (index === 1) {
+        document.querySelector("#click1").style.backgroundColor = '#FF5029'
+        document.querySelector("#click1").style.color = 'white'
+        document.querySelector("#click2").style.backgroundColor = 'unset'
+        document.querySelector("#click2").style.color = 'unset'
+        document.querySelector("#click3").style.backgroundColor = 'unset'
+        document.querySelector("#click3").style.color = 'unset'
+        document.querySelector("#click4").style.backgroundColor = 'unset'
+        document.querySelector("#click4").style.color = 'unset'
+    }
+
+    else if (index === 2) {
+        document.querySelector("#click2").style.backgroundColor = '#FF5029'
+        document.querySelector("#click2").style.color = 'white'
+        document.querySelector("#click1").style.backgroundColor = 'unset'
+        document.querySelector("#click1").style.color = 'unset'
+        document.querySelector("#click3").style.backgroundColor = 'unset'
+        document.querySelector("#click3").style.color = 'unset'
+        document.querySelector("#click4").style.backgroundColor = 'unset'
+        document.querySelector("#click4").style.color = 'unset'
+    }
+
+    else if (index === 3) {
+        document.querySelector("#click3").style.backgroundColor = '#FF5029'
+        document.querySelector("#click3").style.color = 'white'
+        document.querySelector("#click2").style.backgroundColor = 'unset'
+        document.querySelector("#click2").style.color = 'unset'
+        document.querySelector("#click1").style.backgroundColor = 'unset'
+        document.querySelector("#click1").style.color = 'unset'
+        document.querySelector("#click4").style.backgroundColor = 'unset'
+        document.querySelector("#click4").style.color = 'unset'
+    }
+
+    else if (index === 4) {
+        document.querySelector("#click4").style.backgroundColor = '#FF5029'
+        document.querySelector("#click4").style.color = 'white'
+        document.querySelector("#click2").style.backgroundColor = 'unset'
+        document.querySelector("#click2").style.color = 'unset'
+        document.querySelector("#click3").style.backgroundColor = 'unset'
+        document.querySelector("#click3").style.color = 'unset'
+        document.querySelector("#click1").style.backgroundColor = 'unset'
+        document.querySelector("#click1").style.color = 'unset'
+    }
+}
+
+this.style.backgroundColor = '#FF5029', this.style.color = 'white';
+
+
+function Clickbtn(index) {
+
+    if (index === 1) {
+        document.querySelector(".click1").style.backgroundColor = '#17a2b8'
+        document.querySelector(".click1").style.color = 'white'
+        document.querySelector(".click2").style.backgroundColor = 'unset'
+        document.querySelector(".click2").style.color = 'unset'
+        document.querySelector(".click3").style.backgroundColor = 'unset'
+        document.querySelector(".click3").style.color = 'unset'
+        document.querySelector(".click4").style.backgroundColor = 'unset'
+        document.querySelector(".click4").style.color = 'unset'
+    }
+
+    else if (index === 2) {
+        document.querySelector(".click2").style.backgroundColor = '#17a2b8'
+        document.querySelector(".click2").style.color = 'white'
+        document.querySelector(".click1").style.backgroundColor = 'unset'
+        document.querySelector(".click1").style.color = 'unset'
+        document.querySelector(".click3").style.backgroundColor = 'unset'
+        document.querySelector(".click3").style.color = 'unset'
+        document.querySelector(".click4").style.backgroundColor = 'unset'
+        document.querySelector(".click4").style.color = 'unset'
+    }
+
+    else if (index === 3) {
+        document.querySelector(".click3").style.backgroundColor = '#17a2b8'
+        document.querySelector(".click3").style.color = 'white'
+        document.querySelector(".click2").style.backgroundColor = 'unset'
+        document.querySelector(".click2").style.color = 'unset'
+        document.querySelector(".click1").style.backgroundColor = 'unset'
+        document.querySelector(".click1").style.color = 'unset'
+        document.querySelector(".click4").style.backgroundColor = 'unset'
+        document.querySelector(".click4").style.color = 'unset'
+    }
+
+    else if (index === 4) {
+        document.querySelector(".click4").style.backgroundColor = '#17a2b8'
+        document.querySelector(".click4").style.color = 'white'
+        document.querySelector(".click2").style.backgroundColor = 'unset'
+        document.querySelector(".click2").style.color = 'unset'
+        document.querySelector(".click3").style.backgroundColor = 'unset'
+        document.querySelector(".click3").style.color = 'unset'
+        document.querySelector(".click1").style.backgroundColor = 'unset'
+        document.querySelector(".click1").style.color = 'unset'
+    }
+}
+
+this.style.backgroundColor = '#17a2b8', this.style.color = 'white'
+
+
+
+
